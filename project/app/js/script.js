54f7b54c056eb0ff5ceff69a325568a6d7b7c34c
Railson Santiago <railsonsantiago123@gmail.com>
2021-08-25 21:43:32 -0300

    Animated Menu part 1

--- project/app/js/script.js
+++ project/app/js/script.js
@@ -1,1 +1,11 @@
-console.log("hello")
+const menu = document.querySelector("#btnMenu");
+
+menu.addEventListener('click', function() {
+    console.log('open menu')
+
+    if(menu.classList.contains("open")) {
+        menu.classList.remove('open')
+    }else {
+        menu.classList.add('open')
+    }
+})
